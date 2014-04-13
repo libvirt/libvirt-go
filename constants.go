b24b0f3fbd787d4922a61950aebb5cdb0d2181d0
Alex Zorin <alex@zor.io>
2014-04-13 21:05:00 +1000

    start moving consts to constants.go

---
+++ constants.go
@@ -0,0 +1,21 @@
+package libvirt
+
+/*
+#cgo LDFLAGS: -lvirt -ldl
+#include <libvirt/libvirt.h>
+#include <libvirt/virterror.h>
+#include <stdlib.h>
+*/
+import "C"
+
+// virDomainState
+const (
+	VIR_DOMAIN_NOSTATE     = C.VIR_DOMAIN_NOSTATE
+	VIR_DOMAIN_RUNNING     = C.VIR_DOMAIN_RUNNING
+	VIR_DOMAIN_BLOCKED     = C.VIR_DOMAIN_BLOCKED
+	VIR_DOMAIN_PAUSED      = C.VIR_DOMAIN_PAUSED
+	VIR_DOMAIN_SHUTDOWN    = C.VIR_DOMAIN_SHUTDOWN
+	VIR_DOMAIN_CRASHED     = C.VIR_DOMAIN_CRASHED
+	VIR_DOMAIN_PMSUSPENDED = C.VIR_DOMAIN_PMSUSPENDED
+	VIR_DOMAIN_SHUTOFF     = C.VIR_DOMAIN_SHUTOFF
+)
