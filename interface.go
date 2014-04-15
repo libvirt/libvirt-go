9c02d8e4bc6560352d6d92c555af62d8fcc62ab7
dorzheh <dmitry.orzhehovsky@gmail.com>
2014-04-14 22:33:28 -0700

    Adding new files: interface.go and interface_test.go

---
+++ interface.go
@@ -0,0 +1,16 @@
+package libvirt
+
+/*
+#cgo LDFLAGS: -lvirt -ldl
+#include <libvirt/libvirt.h>
+#include <libvirt/virterror.h>
+#include <stdlib.h>
+*/
+import "C"
+
+
+
+type VirInterface struct {
+	ptr C.virInterfacePtr
+}
+
