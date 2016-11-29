0b4afe553632204ad44e202f234c24311f0ad10a
Daniel P. Berrange <berrange@redhat.com>
2016-11-29 11:14:20 +0000

    Add remaining enum constants for event APIs
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- events.go
+++ events.go
@@ -7,6 +7,15 @@ package libvirt
 */
 import "C"
 
+type VirEventHandleType int
+
+const (
+	VIR_EVENT_HANDLE_READABLE = VirEventHandleType(C.VIR_EVENT_HANDLE_READABLE)
+	VIR_EVENT_HANDLE_WRITABLE = VirEventHandleType(C.VIR_EVENT_HANDLE_WRITABLE)
+	VIR_EVENT_HANDLE_ERROR    = VirEventHandleType(C.VIR_EVENT_HANDLE_ERROR)
+	VIR_EVENT_HANDLE_HANGUP   = VirEventHandleType(C.VIR_EVENT_HANDLE_HANGUP)
+)
+
 func EventRegisterDefaultImpl() error {
 	if i := int(C.virEventRegisterDefaultImpl()); i != 0 {
 		return GetLastError()
