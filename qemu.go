d434c37342e4616fbe919124c812a95bbdead17d
Daniel P. Berrange <berrange@redhat.com>
2017-04-26 16:53:33 +0100

    Add support for virConnectDomainQemuMonitorEvent APIs
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- qemu.go
+++ qemu.go
@@ -38,6 +38,7 @@ package libvirt
 #include <libvirt/virterror.h>
 #include <stdlib.h>
 #include "qemu_compat.h"
+#include "qemu_cfuncs.h"
 */
 import "C"
 
@@ -71,6 +72,13 @@ const (
 	DOMAIN_QEMU_AGENT_COMMAND_DEFAULT  = DomainQemuAgentCommandTimeout(C.VIR_DOMAIN_QEMU_AGENT_COMMAND_DEFAULT)
 	DOMAIN_QEMU_AGENT_COMMAND_NOWAIT   = DomainQemuAgentCommandTimeout(C.VIR_DOMAIN_QEMU_AGENT_COMMAND_NOWAIT)
 	DOMAIN_QEMU_AGENT_COMMAND_SHUTDOWN = DomainQemuAgentCommandTimeout(C.VIR_DOMAIN_QEMU_AGENT_COMMAND_SHUTDOWN)
+)
+
+type DomainQemuMonitorEventFlags int
+
+const (
+	CONNECT_DOMAIN_QEMU_MONITOR_EVENT_REGISTER_REGEX  = DomainQemuMonitorEventFlags(C.VIR_CONNECT_DOMAIN_QEMU_MONITOR_EVENT_REGISTER_REGEX)
+	CONNECT_DOMAIN_QEMU_MONITOR_EVENT_REGISTER_NOCASE = DomainQemuMonitorEventFlags(C.VIR_CONNECT_DOMAIN_QEMU_MONITOR_EVENT_REGISTER_NOCASE)
 )
 
 func (d *Domain) QemuMonitorCommand(command string, flags DomainQemuMonitorCommandFlags) (string, error) {
@@ -109,4 +117,66 @@ func (c *Connect) DomainQemuAttach(pid uint32, flags uint32) (*Domain, error) {
 		return nil, GetLastError()
 	}
 	return &Domain{ptr: ptr}, nil
+}
+
+type DomainQemuMonitorEvent struct {
+	Event   string
+	Seconds int64
+	Micros  uint
+	Details string
+}
+
+type DomainQemuMonitorEventCallback func(c *Connect, d *Domain, event *DomainQemuMonitorEvent)
+
+//export domainQemuMonitorEventCallback
+func domainQemuMonitorEventCallback(c C.virConnectPtr, d C.virDomainPtr,
+	event *C.char, seconds C.longlong, micros C.uint, details *C.char, goCallbackId int) {
+
+	domain := &Domain{ptr: d}
+	connection := &Connect{ptr: c}
+
+	eventDetails := &DomainQemuMonitorEvent{
+		Event:   C.GoString(event),
+		Seconds: int64(seconds),
+		Micros:  uint(micros),
+		Details: C.GoString(details),
+	}
+
+	callbackFunc := getCallbackId(goCallbackId)
+	callback, ok := callbackFunc.(DomainQemuMonitorEventCallback)
+	if !ok {
+		panic("Inappropriate callback type called")
+	}
+	callback(connection, domain, eventDetails)
+
+}
+
+func (c *Connect) DomainQemuMonitorEventRegister(dom *Domain, event string, callback DomainQemuMonitorEventCallback, flags DomainQemuMonitorEventFlags) (int, error) {
+	cEvent := C.CString(event)
+	defer C.free(unsafe.Pointer(cEvent))
+	goCallBackId := registerCallbackId(callback)
+
+	callbackPtr := unsafe.Pointer(C.domainQemuMonitorEventCallback_cgo)
+	var cdom C.virDomainPtr
+	if dom != nil {
+		cdom = dom.ptr
+	}
+	ret := C.virConnectDomainQemuMonitorEventRegister_cgo(c.ptr, cdom,
+		cEvent,
+		C.virConnectDomainEventGenericCallback(callbackPtr),
+		C.long(goCallBackId),
+		C.uint(flags))
+	if ret == -1 {
+		freeCallbackId(goCallBackId)
+		return 0, GetLastError()
+	}
+	return int(ret), nil
+}
+
+func (c *Connect) DomainQemuEventDeregister(callbackId int) error {
+	// Deregister the callback
+	if i := int(C.virConnectDomainQemuMonitorEventDeregister(c.ptr, C.int(callbackId))); i != 0 {
+		return GetLastError()
+	}
+	return nil
 }
