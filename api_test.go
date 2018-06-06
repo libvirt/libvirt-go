49ad278e6f7efd54a3737b7f1fdc6264dfae9470
Daniel P. Berrangé <berrange@redhat.com>
2018-06-06 16:47:59 +0100

    Blacklist virGetLastError{Code,Domain}
    These methods will not be exposed to apps, since we always return
    all errors.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- api_test.go
+++ api_test.go
@@ -69,6 +69,8 @@ var (
 		"virCopyLastError",
 		"virFreeError",
 		"virGetLastErrorMessage",
+		"virGetLastErrorCode",
+		"virGetLastErrorDomain",
 		"virResetLastError",
 		"virSaveLastError",
 		"virDefaultErrorFunc",
