0b92ba79f4d36e54e53269ae7276c3f69ee0a670
kolcak <kolcakmilan@gmail.com>
2019-10-01 14:15:28 +0200

    Revert "Add support for virNetworkPort object and APIs"
    This reverts commit 4bc570bf9d9c9881275271d46267757be93a9f21.

--- network_wrapper.h
+++ network_wrapper.h
@@ -30,7 +30,6 @@
 #include <libvirt/libvirt.h>
 #include <libvirt/virterror.h>
 #include "network_compat.h"
-#include "network_port_compat.h"
 
 int
 virNetworkCreateWrapper(virNetworkPtr network,
@@ -115,28 +114,6 @@ virNetworkUpdateWrapper(virNetworkPtr network,
                         const char *xml,
                         unsigned int flags,
                         virErrorPtr err);
-
-virNetworkPortPtr
-virNetworkPortLookupByUUIDWrapper(virNetworkPtr net,
-				  const unsigned char *uuid,
-				  virErrorPtr err);
-
-virNetworkPortPtr
-virNetworkPortLookupByUUIDStringWrapper(virNetworkPtr net,
-					const char *uuidstr,
-					virErrorPtr err);
-
-virNetworkPortPtr
-virNetworkPortCreateXMLWrapper(virNetworkPtr net,
-			       const char *xmldesc,
-			       unsigned int flags,
-			       virErrorPtr err);
-
-int
-virNetworkListAllPortsWrapper(virNetworkPtr network,
-			      virNetworkPortPtr **ports,
-			      unsigned int flags,
-			      virErrorPtr err);
 
 
 #endif /* LIBVIRT_GO_NETWORK_WRAPPER_H__ */
